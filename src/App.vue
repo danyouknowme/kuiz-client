12cf32cb450175f6fa36283c52ffcd40a92b1780
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2021-09-19 20:01:36 +0700

    remake app for able to link to another page

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <template>
   <div class="app">
-    <navbar />
-    <home />
+    <router-view />
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar'
-import Home from '@/components/Home'
 
 export default {
   name: 'App',
-  components: {
-    Navbar,
-    Home
-  }
 }
 </script>
 
